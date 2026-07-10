c92e2e15f0f87724a0e512d805a5d2ced7a02408
ABHINASH SAHOO <abinashsahoo626@gmail.com>
2026-07-10 15:04:31 +0530

    new file

--- JDBC/src/main/java/org/example/App.java
+++ JDBC/src/main/java/org/example/App.java
@@ -1,9 +1,21 @@
 package org.example;
 
-public class App 
+
+import org.example.entity.Student;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello!" );
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        Student s1=context.getBean(Student.class);
+
+        System.out.println(s1.getName()+" "+s1.getAddress());
     }
+
+
+
 }
