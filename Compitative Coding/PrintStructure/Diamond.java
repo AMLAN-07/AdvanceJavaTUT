c754d2f48be18e2c420385bd222cdf18bca5d8ab
ABHINASH SAHOO <abinashsahoo626@gmail.com>
2026-07-14 10:09:24 +0530

    Print Diamond pattern

---
+++ Compitative Coding/PrintStructure/Diamond.java
@@ -0,0 +1,31 @@
+package PrintStructure;
+
+public class Diamond {
+    public void printDiamond(int rows) {
+        for(int i=rows-1; i<=rows; i++) {
+            for(int j=i; j<rows; j++) {
+                System.out.print(" ");
+            }
+            for(int k=1; k<=(2*i-1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=rows-1; i>=1; i--) {
+            for(int j=i; j<rows; j++) {
+                System.out.print(" ");
+            }
+            for(int k=1; k<=(2*i-1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+        
+        
+    }
+    public static void main(String[] args) {
+        Diamond d = new Diamond();
+        d.printDiamond(5);
+    }
+}
