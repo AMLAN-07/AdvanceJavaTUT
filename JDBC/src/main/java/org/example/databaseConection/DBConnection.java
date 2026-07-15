75bbfef37b407ba06353e2d238cd220aaa47e0ca
ABHINASH SAHOO <abinashsahoo626@gmail.com>
2026-07-15 10:24:07 +0530

    Connect to SQL function

---
+++ JDBC/src/main/java/org/example/databaseConection/DBConnection.java
@@ -0,0 +1,22 @@
+package org.example.databaseConection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+
+    public static Connection getConnection() {
+        try {
+            String url = "jdbc:mysql://localhost:3306/jdbc";
+            String user = "root";
+            String password = "hzkk4567@A";
+
+            return DriverManager.getConnection(url, user, password);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
+}
